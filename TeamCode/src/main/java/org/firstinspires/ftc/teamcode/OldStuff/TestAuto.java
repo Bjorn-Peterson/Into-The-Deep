622c3fe0711fdd787a429430471d5be80fc9cf45
Bjorn Peterson <bjbjorn@gmail.com>
2025-02-10 21:25:30 -0600

    WR optimization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OldStuff/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OldStuff/TestAuto.java
@@ -8,12 +8,14 @@ import com.acmerobotics.roadrunner.TurnConstraints;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.NewRobot.Lift;
 
 
 @Config
+@Disabled
 @Autonomous(name = "test", group = "Autonomous")
 public class TestAuto extends LinearOpMode {
 
